57a3405e880d140b6fe44b38c9c9546d249482b2
Mauro Gestoso <maurogestoso@gmail.com>
2022-10-19 13:33:44 +0200

    Add base ESLint config

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  plugins: [],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {},
+};
